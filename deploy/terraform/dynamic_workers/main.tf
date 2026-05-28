eee8c4247edd68e4dae00e5651a6cc2d4f7313c7
kirilldorr <doropij@gmail.com>
2026-05-28 13:39:17 +0300

    Fix k3s-agent install on worker node

--- deploy/terraform/dynamic_workers/main.tf
+++ deploy/terraform/dynamic_workers/main.tf
@@ -65,13 +65,14 @@ resource "aws_instance" "worker" {
     echo '      username: AWS' >> /usr/local/bin/update-ecr-token.sh
     echo '      password: "$TOKEN"' >> /usr/local/bin/update-ecr-token.sh
     echo 'YAML' >> /usr/local/bin/update-ecr-token.sh
-    
-    echo 'systemctl restart k3s-agent' >> /usr/local/bin/update-ecr-token.sh
+
+    echo 'systemctl restart k3s-agent || true' >> /usr/local/bin/update-ecr-token.sh
     chmod +x /usr/local/bin/update-ecr-token.sh
     /usr/local/bin/update-ecr-token.sh
     echo "0 */10 * * * root /usr/local/bin/update-ecr-token.sh > /var/log/ecr-cron.log 2>&1" > /etc/cron.d/update-ecr-token
     curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="${var.k3s_version}" K3S_URL="https://${data.terraform_remote_state.base.outputs.public_ip}:6443" K3S_TOKEN="${var.k3s_token}" sh -
     EOF
+
   lifecycle {
     create_before_destroy = true
     ignore_changes        = [ami]
